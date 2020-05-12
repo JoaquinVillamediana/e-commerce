b25c4267cbb5742df5e654026ac1f743bd2239f3
joaquin <joacovillamediana@gmail.com>
2020-05-12 21:06:05 +0000

    Vista de Registro

--- resources/views/frontend/register/index.blade.php
+++ resources/views/frontend/register/index.blade.php
@@ -13,36 +13,102 @@
         @endif
 </div>
 
-    <form method="POST" action="{{ route('login') }}" style="display:none" id="form-login" class="row mt-4">
-        {{csrf_field()}}
-        <div class="col-7 m-auto">
-            <div class="form-group">
-                <label class="text-light " for="exampleInputEmail1">Email address</label>
-                <input type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" id="email" placeholder="example@example.com" aria-describedby="emailHelp">
-                @if ($errors->has('email'))
-                    <span class="invalid-feedback" role="alert">
-                        <strong>Credenciales no v&aacute;lidas.</strong>
-                    </span>
-                    @endif  
-              </div>
-        </div>
-        <div class="col-7 m-auto">
-            <div class="form-group">
-                <label class="text-light" for="exampleInputPass1">Contraseña</label>
-                <input id="password" type="password" name="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}"  aria-describedby="emailHelp">
-                <small id="emailHelp" class=" text-light form-text text-muted">Nunca compartiremos tus datos personales.</small>
-                @if ($errors->has('password'))
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $errors->first('password') }}</strong>
-                    </span>
-                    @endif
-            </div>
+<form method="POST" style="display: none;" id="form_register" action="{{ route('user.store') }}" role="form"
+enctype="multipart/form-data">
+{{ csrf_field() }}
 
-        </div>
+
+
+<div class="row">
+
+    <div class="form-group col col-md-3 offset-md-3">
+        <label class="text-light">Nombre</label>
+        <input id="name" name="name" maxlength="60"
+            class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}"
+            placeholder="Nombre" value="{{ old('name') }}">
+        @if ($errors->has('name'))
+        <span class="invalid-feedback" role="alert">
+            <strong>Debe ingresar un nombre.</strong>
+        </span>
+        @endif
+    </div>
+
+    <div class="form-group col col-md-3 ">
+        <label class="text-light">Apellido</label>
+        <input id="last_name" name="last_name" maxlength="60"
+            class="form-control{{ $errors->has('last_name') ? ' is-invalid' : '' }}"
+            placeholder="Apellido" value="{{ old('last_name') }}">
+        @if ($errors->has('last_name'))
+        <span class="invalid-feedback" role="alert">
+            <strong>Debe ingresar un apellido.</strong>
+        </span>
+        @endif
+    </div>
+
+    
+
+</div>
+<div class="row">
+<div class="form-group col col-md-3 offset-md-3">
+    <label class="text-light">Email</label>
+    <input id="email" name="email" maxlength="60"
+        class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}"
+        placeholder="Email" value="{{ old('email') }}">
+    @if ($errors->has('email'))
+    <span class="invalid-feedback" role="alert">
+        <strong>Debe ingresar un email.</strong>
+    </span>
+    @endif
+    @if ($errors->has('duplicated_email_error'))
+    <span class="invalid-feedback" role="alert" style="display:block;">
+        <strong>El email ingresado ya se encuentra registrado.</strong>
+    </span>
+    @endif
+</div>
+
+
+<div class="form-group col col-md-3 ">
+    <label class="text-light">Teléfono</label>
+    <input id="phone" name="phone" maxlength="60"
+        class="form-control{{ $errors->has('phone') ? ' is-invalid' : '' }}"
+        placeholder="Teléfono" value="{{ old('phone') }}">
+    @if ($errors->has('phone'))
+    <span class="invalid-feedback" role="alert">
+        <strong>Debe ingresar un teléfono.</strong>
+    </span>
+    @endif
+</div>
+</div>
+<div class="row">
+<div class="form-group col-md-6 offset-md-3" id="box_password">
+    <label class="text-light">Contraseña</label>
+    <input type="password" id="password" name="password"
+        class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}"
+        placeholder="Password" value="{{ old('password') }}">
+    @if ($errors->has('password'))
+    <span class="invalid-feedback" role="alert">
+        <strong>Debe ingresar un password (min. 8 caracteres).</strong>
+    </span>
+    @endif
+</div>
+</div>
+<div class="row">
+    <div class="form-group col-md-6 offset-md-3" id="box_password">
+        <label class="text-light">Confirmar Contraseña</label>
+        <input type="password" id="password" name="password"
+            class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}"
+            placeholder="Password" value="{{ old('password') }}">
+        @if ($errors->has('password'))
+        <span class="invalid-feedback" role="alert">
+            <strong>Debe ingresar un password (min. 8 caracteres).</strong>
+        </span>
+        @endif
+    </div>
+    </div>
         <div class="col-2 offset-md-5">
             
             <button type="submit" class="btn-block btn btn-dark " style="background-color: #303F9F; border:#303F9F;" >
-                {{ __('Login') }}
+                Registrarme
             </button>
         </div>
 
@@ -52,7 +118,7 @@
 
 <script>
 $( document ).ready(function() {
-    $('#form-login').fadeIn(1000);
+    $('#form_register').fadeIn(1000);
 });
 </script>
 
