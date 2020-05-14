03e57d9934c7383b0a47b7c41d9c86a5cdc42cef
joaquin <joacovillamediana@gmail.com>
2020-05-14 21:24:51 +0000

    pagina categorias

--- app/Http/Controllers/frontend/CateController.php
+++ app/Http/Controllers/frontend/CateController.php
@@ -23,7 +23,9 @@ class CateController extends Controller {
     public function index($id) {
 
         $aProducts = ProductsModel::where('category_id','=',$id)->get();
-        
+        $category_name = CategoriesModel::select('categories.name')
+        ->where('id','=',$id)
+        ->first();
 
         $aCategories = CategoriesModel::select('categories.*', DB::raw('count(sub_categories.id)  as quantity_sub'))->leftjoin('sub_categories','categories.id','=','sub_categories.category_id')
         ->where('categories.visible', '=', '1')
@@ -32,7 +34,7 @@ class CateController extends Controller {
         $aSubCategories = SubModel::where('sub_categories.visible' ,'=', '1')
         ->get();
         
-        return view('frontend/cate.index',compact('aCategories','aSubCategories','aProducts'));
+        return view('frontend/cate.index',compact('aCategories','aSubCategories','aProducts','category_name'));
     }
 
     public function show() {
