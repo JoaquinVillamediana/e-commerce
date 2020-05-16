bcdad0dc0fbe78c571a9f9bd581ce19223d7d1d2
Gonzalo Brizuela <gonzalobrizuelaortiz@icloud.com>
2020-05-16 13:50:52 +0000

    Cambio vista de busqueda

--- resources/views/frontend/search/index.blade.php
+++ resources/views/frontend/search/index.blade.php
@@ -2,47 +2,36 @@
 
 @section('content')
 <link rel="stylesheet" href="/css/frontend/products.css">
-<div class="mt-2 container-fluid">
+<div class="mt-5 pb-5 container-fluid">
 
 
 
 
 
 
-    <div class="row pt-5 justify-content-center text-right">
-
-      <div class="col col-md-7 mb-2 pt-3 pr-1">
-          <h2 class="text-right">Busqueda</h2>
-      </div>
-        <div class="col pl-0 mb-2">
-  <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
-<lottie-player src="https://assets3.lottiefiles.com/packages/lf20_HirsSZ.json"  background="transparent"  speed="1"  style="width: 70px; height: 70px;"    autoplay></lottie-player>
-
-</div>
-
- </br>
-<div class="row">
 
 @if (!empty($aProducts))
 @foreach ($aProducts as $product)
           
- 
-<div class="col">
+
+
+
+<div class="card">
+
+  <h5 class="card-header">${{$product->price}}</h5>
+  <div class="card-body">
+    <h5 class="card-title">{{$product->name}}</h5>
+    <p class="card-text">{!! $product->description !!}</p>
+    <a href="{{route('product',$product->id)}}" class="btn btn-primary">Comprar</a>
+  </div>
+  
+</div>
+
             
-            <div id="card" class="card" style="width: 18rem;">
-              <a  href="{{route('product',$product->id)}}" id="productBox">
-              <img class="card-img-top" src="/uploads/products/{{$product->image}}" alt="Card image cap">
-              <div class="card-body">
-                <h5 class="card-title">{{$product->name}}</h5>
-                <p class="card-text text-dark">${{$product->price}}</p>
-                
-              </div>
-            </a>
-            </div>
+         
             </br>
  </br>
  </br>
-          </div>
         
         
               @endforeach
@@ -84,9 +73,6 @@
         
               @endforeach
               @endif
-    </div>
-
-
-</div>
+  
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -54,6 +54,7 @@ Route::prefix('admin')->group(function () {
             Route::resource('categories', 'admin\CategoriesController');
             Route::post('category_visible', 'admin\CategoriesController@setCategoryVisible')->name('category_visible');
             Route::post('subcategory_visible', 'admin\SubController@setSubcategoryVisible')->name('subcategory_visible');
+            Route::post('product_visible', 'admin\ProductsController@setProductVisible')->name('product_visible');
            
             
             Route::resource('products', 'admin\ProductsController');
