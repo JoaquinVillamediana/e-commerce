3cd008b8795a6c272c70bf9b4c1a2c3171f60fad
joaquin <joacovillamediana@gmail.com>
2020-05-18 20:03:42 +0000

    Rediseño carrito

--- resources/views/frontend/carrito/index.blade.php
+++ resources/views/frontend/carrito/index.blade.php
@@ -1,7 +1,7 @@
 @extends('frontend/layouts.app')
 
 @section('content')
-<link rel="stylesheet" href="/css/frontend/products.css">
+
 <div class="mt-2 container-fluid">
 
 
@@ -13,40 +13,39 @@
       <div class="col ">
           <h2 class="text-center">{{"MI carrito"}}</h2>
       </div>
-        {{-- <div class="col pl-0 mb-2">
-  <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
-<lottie-player src="https://assets3.lottiefiles.com/packages/lf20_HirsSZ.json"  background="transparent"  speed="1"  style="width: 70px; height: 70px;"    autoplay></lottie-player> --}}
+        
 </div>
-
+<?php $total=0?>
+<div class="row">
 @if (!empty($aProducts))
 @foreach ($aProducts as $product)
           
+<div class="col">
 
 
-
-<div class="card">
+<div class="card mt-3 mb-3">
 
   <h5 class="card-header">${{$product->price}}</h5>
   <div class="card-body">
-    <h5 class="card-title">{{$product->name}}</h5>
+    <h5 class="card-title"> <a href="{{route('product',$product->id)}}">{{$product->name}}</a></h5>
     <p class="card-text">{!! $product->description !!}</p>
-    <a href="{{route('product',$product->id)}}" class="btn btn-primary">Comprar</a>
+    
   </div>
   
 </div>
-
-            
-         
-            </br>
- </br>
- </br>
-        
-        
+</div>
+        <?php $total+=$product->price?>
               @endforeach
-  
+  <div class="col-md-8 col-12 offset-md-2 mt-3 border-top border-dark pt-2">
+    <h5 class="text-center">Total: ${{$total}}</h5>
+  </div>
+  <div class="col-md-8 col-12 offset-md-2 mt-4">
+    <a href="{{route('product',$product->id)}}" class="btn btn-primary btn-block">Comprar</a>
+  </div>
               @endif
   
 
     </div>
+  </div>
 </div>
 @endsection
