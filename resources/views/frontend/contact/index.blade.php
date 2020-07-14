30d46cbc7022122a76fae676a7608da586ef842a
joaquin <joacovillamediana@gmail.com>
2020-07-14 19:50:26 +0000

    Faqs y Contacto

--- resources/views/frontend/contact/index.blade.php
+++ resources/views/frontend/contact/index.blade.php
@@ -1,90 +1,74 @@
 @extends('frontend/layouts.app')
 
 @section('content')
-
+<link rel="stylesheet" href="/css/frontend/contact.css">
 <div class=" container-fluid">
 
+  <div class="row pt-5 mb-5 justify-content-center text-right">
 
+    <div class="col ">
+      <h2 style="font-size: 25px;color:#000;" class="font-weight-bold text-center">Contacto</h2>
 
-
-
-  <div class="row pt-2 justify-content-center text-right">
-    <div class="col-4 d-none d-lg-block d-xl-block d-md-block">
-      <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
-<lottie-player src="https://assets5.lottiefiles.com/temp/lf20_adfZjR.json"  background="transparent"  speed="1"  style="width: 300px; height: 300px;"  loop  autoplay></lottie-player>
     </div>
-    <div class="col-md-4 col-12">
-        <h2 class="text-center" id="headFav" style="    position: absolute;top: 100px;left: 100px;z-index: 2;display:none">CONTACTO</h2>
-        
-    
-    </div>
-    <div class="col-4 d-none d-lg-block d-xl-block d-md-block">
-      <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
-<lottie-player src="https://assets5.lottiefiles.com/temp/lf20_adfZjR.json"  background="transparent"  speed="1"  style="width: 300px; height: 300px;"  loop  autoplay></lottie-player>
-    </div>
+
   </div>
+  <form method="POST" action="{{ route('contact_post') }}" id="contact_form" role="form" enctype="multipart/form-data">
+    {{ csrf_field() }}
+    <div class="row justify-content-center text-left">
 
-  <div class="row justify-content-center text-right">
-  <div class="row justify-content-center text-right">
+
+
+
+
+
+
+
+
+
+      <div class="col-12">
+        <label>¿Que Problema tuviste?</label>
+      </div>
+      <div class="col-12">
+        <select class="problem_select" name="contacts_type" id="contacts_type">
+
+          <option value="1">No llego mi envio</option>
+          <option value="2">Producto fallado</option>
+          <option value="3">No era lo que esperaba</option>
+          <option value="4">Otro</option>
+        </select>
+        @if ($errors->has('category'))
+        <span class="invalid-feedback" role="alert">
+          <strong>Debe seleccionar una problema valido.</strong>
+        </span>
+        @endif
+      </div>
+
+      <div class="col-12">
+        <label>Comentario</label>
+      </div>
+
+      <div class="col-12">
+        <textarea name="description" id="description" cols="30" maxlength="500"  rows="5"></textarea>
+      <span id="description_error" class="invalid-feedback" role="alert" style="display:none;">
+        <strong>Debe ingresar una descripción (max. 500 car.)</strong>
+      </span>
+      </div>
+      <div class="col-12">
+        <button type="submit" class="btn shop-btn">Enviar</button>
+      </div>
       
-                <div class="row justify-content-center text-right">
-                    <div class="col-lg-8 margin-bottom-20" style="margin: 0 auto;">
-                        <form method="POST" action="{{ route('contact_post') }}" id="task_form" role="form" enctype="multipart/form-data">
-                            {{ csrf_field() }}
-                           
-
-                           
-
-                            <div class="row">
-
-                               
-                                    <label>Problema</label>
-                                    <select class="form-control" name="contacts_type" id="contacts_type">
-                                       
-                                            <option value="1">No llego mi envio</option>
-                                            <option value="2">Producto fallado</option>
-                                            <option value="3">No era lo que esperaba</option>
-                                            <option value="4">Otro</option>
-                                    </select>
-                                    @if ($errors->has('category'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>Debe seleccionar una problema valido.</strong>
-                                    </span>
-                                    @endif
-                                  
-
-                                {{-- ajax --}}
-
-                             
-                            </div>
-
-
-                            <div class="form-group">
-                                <label>Comentario</label>
-                              
-                                <input type="text" id="description" name="description"/>
-                                <div id="editor-container" style="border:1px solid #ced4da; border-radius:.25rem; margin-top:5px;">{!! old('description') !!}</div>
-                                <span id="description_error" class="invalid-feedback" role="alert" style="display:none;">
-                                    <strong>Debe ingresar una descripción (max. 12000 car.)</strong>
-                                </span>                                
-                            </div>  
+      
 
 
 
-                           
 
-                           
-                  
+    </div>
+  </form>
 
-                            <button type="submit" class="btn btn-primary">Enviar</button>
-                            <button type="reset" class="btn btn-default">Reset</button>
-                        </form>
-                  
-                </div>
-  </div>
-
-</div>
-<script>$( document ).ready(function() {
+  <script>
+    $( document ).ready(function() {
   $('#headFav').fadeIn(400);
-});</script>
+});
+  </script>
+</div>
 @endsection
